d37e3e2f4b39ae80820b42b63774c43b4272e542
arbinada <arbinada@cfd95cc2-f724-4227-9a96-9a83de4730d4>
2013-03-16 14:50:04 +0000

    Fixed relase script (new repo)

--- create-package.sh
+++ create-package.sh
@@ -1,6 +1,7 @@
 #!bin/sh
 sh ./build-all.sh
 
+svn_repo_url=https://svn.code.sf.net/p/gettextnet/code
 version=1.1
 product_name=GettextNet
 release_dir=GettextNet
@@ -11,11 +12,10 @@ mkdir ./$release_dir/Bin
 mkdir ./$release_dir/Win32
 cp -r ./Bin/* ./$release_dir/Bin/
 cp -r ./Win32/* ./$release_dir/Win32/
+svn checkout -q $svn_repo_url ./$release_dir/
 
-svn_repo_url=https://genielamp.svn.sourceforge.net/svnroot/gettextnet
 revision=$(svn info -rHEAD $svn_repo_url | grep Revision: | cut -c11-)
 echo Last revision is: $revision
-svn checkout -q $svn_repo_url ./$release_dir/
 
 rm $product_name*.zip
 zip -9 -r -q $product_name".v"$version"r"$revision.zip ./$release_dir -x *.svn*
